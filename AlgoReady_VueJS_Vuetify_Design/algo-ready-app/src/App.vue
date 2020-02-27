4d8fae0acbd011a62d5f3160d52017d109874d6a
Stanislav <sheveliukhin.s@gmail.com>
2020-02-28 01:45:39 +0600

    "bad" divs added. Some content added

--- AlgoReady_VueJS_Vuetify_Design/algo-ready-app/src/App.vue
+++ AlgoReady_VueJS_Vuetify_Design/algo-ready-app/src/App.vue
@@ -7,15 +7,15 @@
         </v-col>
       </v-row>
 
-      <v-list dense>
-        <v-list-group prepend-icon="mdi-view-dashboard" value="true">
+      <v-list >
+        <v-list-group prepend-icon="mdi-view-dashboard" value="true" color="#00BFC5">
           <template v-slot:activator>
             <v-list-item-title>Portfolio Analysis</v-list-item-title>
           </template>
 
           <v-list-item>
             <v-list-item-content>
-              <v-list-item-title>Portfolio List</v-list-item-title>
+              <v-list-item-title color="#00BFC5">Portfolio List</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
 
@@ -27,7 +27,7 @@
 
           <v-list-item>
             <v-list-item-content>
-              <v-list-item-title>Portfolio</v-list-item-title>
+              <v-list-item-title>Update / Add</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
 
@@ -40,7 +40,7 @@
 
         <!-- Stocks -->
 
-        <v-list-group prepend-icon="mdi-chart-bar" value="true">
+        <v-list-group prepend-icon="mdi-chart-bar" value="true" color="#00BFC5">
           <template v-slot:activator>
             <v-list-item-title>Stocks</v-list-item-title>
           </template>
@@ -72,7 +72,7 @@
 
         <!-- Investors -->
 
-        <v-list-group prepend-icon="mdi-account" value="true">
+        <v-list-group prepend-icon="mdi-account" value="true" color="#00BFC5">
           <template v-slot:activator>
             <v-list-item-title>Investors</v-list-item-title>
           </template>
@@ -98,7 +98,7 @@
 
         <!-- Market -->
 
-        <v-list-group prepend-icon="mdi-chart-pie" value="true">
+        <v-list-group prepend-icon="mdi-chart-pie" value="true" color="#00BFC5">
           <template v-slot:activator>
             <v-list-item-title>Market</v-list-item-title>
           </template>
@@ -118,14 +118,14 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app clipped-right>
-      <v-toolbar>
+    <v-app-bar app clipped-right flat>
+      <v-toolbar flat>
         <v-text-field
           label="Search"
           prepend-icon="mdi-magnify"
           dense
           flat
-          
+          id="searchField"
         ></v-text-field>
 
         <v-spacer></v-spacer>
@@ -157,7 +157,33 @@
     </v-app-bar>
 
     <v-content>
-      <v-container class="fill-height" fluid> </v-container>
+      <v-container>
+        <h2>Algoready european Small caps</h2>
+        <p>Last Updated: 20/08/2019</p>
+        <div class="my-2">
+          <v-btn small color="#00BFC5">Edit Portfolio</v-btn>
+        </div>  
+
+      <div id="four-div-wrapper">
+        <div>
+          <p class="blackText">Total Market Value</p>
+          <p>$ 2589647,531</p>
+        </div>
+        <div>
+          <p class="blackText">Total Potential Pressure</p>
+          <p>$1 Billion</p>
+        </div> 
+        <div>
+          <p class="blackText">Days to Trade</p>
+          <p>20</p>
+        </div> 
+        <div>
+          <p class="blackText">Potential Market Impact</p>
+          <p>78%</p>
+        </div>
+      </div>
+
+      </v-container>
     </v-content>
 
     <v-footer app>
@@ -181,5 +207,33 @@ export default {
 <style scoped>
 #logoRow {
   border-bottom: 1px solid #313c4e;
+  padding-bottom: 10px;
+  padding-top: 10px;
 }
+
+#four-div-wrapper{
+  display: flex;
+  justify-content: space-around;
+}
+
+#four-div-wrapper div{
+  width: 180px;
+  height: 80px;
+  min-width: 180px;
+  background: linear-gradient(180deg, #3B98EE 0%, #2CA9CD 100%);
+}
+
+#four-div-wrapper > div > p {
+  text-align: center;
+  padding: 0 !important;
+}
+
+.blackText{
+  color: black;
+}
+
+/* #searchField{
+  padding-top: 10px !important;
+} */
+
 </style>
