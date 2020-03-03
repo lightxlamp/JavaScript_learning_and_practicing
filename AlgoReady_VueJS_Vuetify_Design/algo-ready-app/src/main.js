3c373b1cceddb0596ba872753cb94ea8704f054a
lightxlamp <sheveliukhin.s@gmail.com>
2020-03-04 04:49:12 +0600

    Upwork simple task

--- AlgoReady_VueJS_Vuetify_Design/algo-ready-app/src/main.js
+++ AlgoReady_VueJS_Vuetify_Design/algo-ready-app/src/main.js
@@ -30,35 +30,40 @@ Vue.component('line-chart', {
       datasets: [
         {
           label: "EU Tech",
-          data: [9, 58, 5, 10, 5, 1, 9, 3, 2, 25, 1, 1],
+          //data: [9, 58, 5, 10, 5, 1, 9, 3, 2, 25, 1, 1],
+          data: [9, 58, 5, 10, 5, 1, -9, -58, -5, -10, -5, -1],
           backgroundColor: "#512DA8",
           hoverBackgroundColor: "#512DA8",
           hoverBorderWidth: 0
         },
         {
           label: "Japan Value",
-          data: [15, 52, 12, 5, 4, 0, 4, 2, 1, 22, 3, 2],
+          //data: [15, 52, 12, 5, 4, 0, 4, 2, 1, 22, 3, 2],
+          data: [15, 52, 12, 5, 4, 0, -15, -52, -12, -5, -4, 0],
           backgroundColor: "#FFA000",
           hoverBackgroundColor: "#FFA000",
           hoverBorderWidth: 0
         },
         {
           label: "Utilities",
-          data: [17, 11, 22, 18, 12, 22, 22, 19, 24, 9, 8, 1],
+          //data: [17, 11, 22, 18, 12, 22, 22, 19, 24, 9, 8, 1],
+          data: [17, 11, 22, 18, 12, 22, -17, -11, -22, -18, -12, -22],
           backgroundColor: "#D32F2F",
           hoverBackgroundColor: "#D32F2F",
           hoverBorderWidth: 0
         },
         {
           label: "US Momentum",
-          data: [18, 13, 25, 10, 13, 25, 25, 21, 20, 1, 1, 9],
+          //data: [18, 13, 25, 10, 13, 25, 25, 21, 20, 1, 1, 9],
+          data: [18, 13, 25, 10, 13, 25, -18, -13, -25, -10, -13, -25],
           backgroundColor: "#4D96BE",
           hoverBackgroundColor: "#4D96BE",
           hoverBorderWidth: 0
         },
         {
           label: "Others",
-          data: [20, 15, 7, 7, 1, 29, 24, 22, 1, 9, 9, 0],
+          //data: [20, 15, 7, 7, 1, 29, 24, 22, 1, 9, 9, 0],
+          data: [20, 15, 7, 7, 1, 29, -20, -15, -7, -7, -1, -29],
           backgroundColor: "#F47A7A",
           hoverBackgroundColor: "#F47A7A",
           hoverBorderWidth: 0
@@ -83,7 +88,9 @@ Vue.component('pie-chart', {
         legend: {
           labels: {
             fontColor: 'red'
-          }
+          },
+          fontColor: 'red'
+          
         }
       },
       datasets: [
---
+++ Vue-project-for-tests/vue-app-for-all-tests/src/components/HelloWorld.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="hello">
+    <div class="inline-flex w-full max-w-3xl mx-auto">
+      <div class="ml-auto rounded-full px-5 py-1 bg-coral-100 border border-coral-300">
+        <span class="text-sm mr-3 font-medium">Word allowance:</span>
+        <span class="text-sm font-medium">{{enteredWords}} / {{wordAllowance}}</span>
+      </div>
+    </div>
+    <div>{{content}}</div>
+    <textarea name id cols="30" rows="10" v-model="content" @keydown="wordsLimitHandler" @paste="wordsLimitHandler"></textarea> 
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: "",
+      wordCount: 0,
+      wordAllowance: 999,
+      limitReached: false,
+      membershipCharacterLimit: "",
+      form: {
+        description: ""
+      }
+    };
+  },
+  // watch: {
+  //   form: {
+  //     handler: function() {
+  //       const str = this.form.description;
+  //       const count = str.split(" ").length;
+
+  //       const allowance = this.wordAllowance;
+
+  //       // this needs to count the number of words
+
+  //       // if the number of words is above allowance then trim from end.
+
+  //       // if copy and paste content add, trim from end.
+  //     },
+  //     deep: true,
+  //     immediate: true
+  //   }
+  // },
+  mounted() {
+    this.fetch();
+  },
+  computed: {
+    enteredWords: function() {
+      if(this.content.split(" ").length === 1)
+        return "0"
+      else return this.content.split(" ").length
+    }
+  },
+  methods: {
+    wordsLimitHandler(){
+      if(this.content.split(' ').length > 5)
+      {
+        alert("5 words limit exceeded")
+        console.log(this.content.split(' '));
+        let onlyLimitedNumberOfWords = this.content.split(' ').slice(0, 5); 
+        this.content = onlyLimitedNumberOfWords.join(' ') + " ";
+      }    
+    },
+    async fetch() {
+      /*  axios.get(this.user.uuid + '/vendor/description').then((res) => {
+                    this.checkResponse(res.status)
+                    this.form.description = res.data.data.description
+                })*/
+    },
+    save() {
+      /* this.updating = true
+                axios.patch(this.user.uuid + '/vendor/description', this.form).then((res) => {
+                    if (res.status === 200) {
+
+                        this.fetch()
+                        this.update_successful = true
+
+                        var self = this
+
+                        setTimeout(function () {
+                            self.update_successful = false;
+                        }, 2000);
+                        this.updating = false
+                    }
+
+                })*/
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
