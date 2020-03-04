d5d109ca4ed2cec8ab7ae21dda3e6aaaa3ce466e
lightxlamp <sheveliukhin.s@gmail.com>
2020-03-04 06:39:50 +0600

    Enter disabled. Ome more try with 6th word

--- Vue-project-for-tests/vue-app-for-all-tests/src/components/HelloWorld.vue
+++ Vue-project-for-tests/vue-app-for-all-tests/src/components/HelloWorld.vue
@@ -3,11 +3,11 @@
     <div class="inline-flex w-full max-w-3xl mx-auto">
       <div class="ml-auto rounded-full px-5 py-1 bg-coral-100 border border-coral-300">
         <span class="text-sm mr-3 font-medium">Word allowance:</span>
-        <span class="text-sm font-medium">{{enteredWords}} / {{wordAllowance}}</span>
+        <span class="text-sm font-medium">{{wordCount}} / 5</span>
       </div>
     </div>
     <div>{{content}}</div>
-    <textarea name id cols="30" rows="10" v-model="content" @keydown="wordsLimitHandler" @paste="wordsLimitHandler"></textarea> 
+    <textarea name id cols="30" rows="10" v-model="content" @keypress.13.prevent @change="wordsLimitHandler" @keydown="wordsLimitHandler" @paste="wordsLimitHandler"></textarea> 
   </div>
 </template>
 
@@ -16,7 +16,6 @@ export default {
   data() {
     return {
       content: "",
-      wordCount: 0,
       wordAllowance: 999,
       limitReached: false,
       membershipCharacterLimit: "",
@@ -47,21 +46,26 @@ export default {
     this.fetch();
   },
   computed: {
-    enteredWords: function() {
-      if(this.content.split(" ").length === 1)
-        return "0"
-      else return this.content.split(" ").length
+    phpReadyEnteredText: function() {
+      return "<p>" + this.content + "</p>";
+    },
+    wordCount: function() {
+      let wordsInForm = this.content.split(" ").length;
+      if(wordsInForm > this.wordAllowance){
+        this.content.replace(/\s?$/,''); // remove one space at the end. If limit exceeded
+      }
+      else return wordsInForm
     }
   },
   methods: {
     wordsLimitHandler(){
       if(this.content.split(' ').length > 5)
       {
-        alert("5 words limit exceeded")
+        console.log('phpReadyEnteredText', this.phpReadyEnteredText);
         console.log(this.content.split(' '));
         let onlyLimitedNumberOfWords = this.content.split(' ').slice(0, 5); 
-        this.content = onlyLimitedNumberOfWords.join(' ') + " ";
-      }    
+        this.content = onlyLimitedNumberOfWords.join(' ');
+      }
     },
     async fetch() {
       /*  axios.get(this.user.uuid + '/vendor/description').then((res) => {
@@ -70,6 +74,7 @@ export default {
                 })*/
     },
     save() {
+      let textToDB = this.phpReadyEnteredText;
       /* this.updating = true
                 axios.patch(this.user.uuid + '/vendor/description', this.form).then((res) => {
                     if (res.status === 200) {
