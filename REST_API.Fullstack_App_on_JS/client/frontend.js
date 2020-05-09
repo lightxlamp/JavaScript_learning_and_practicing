fddda054846019bdd1c4e0e25716e426fe0ad475
lightxlamp <sheveliukhin.s@gmail.com>
2020-05-09 07:51:17 +0600

    Client complete

--- REST_API.Fullstack_App_on_JS/client/frontend.js
+++ REST_API.Fullstack_App_on_JS/client/frontend.js
@@ -7,15 +7,38 @@ new Vue({
             form: {
                 name: '',
                 value: ''
-            }
+            },
+            contacts: [
+                {id: 1, name: 'Stanislav', value: '+996-555-40-24-25', isMarked: false},
+                {id: 2, name: 'Adilet', value: '+996-777-99-77-00', isMarked: true}
+            ]
         }
     },
+
+    computed: {
+        isDataValid() {
+            // trim() защитит от пробелов ) ... Капара на таком ловил в Highlanders :)
+            return this.form.value.trim() && this.form.name.trim();
+        }
+    },
+        
     methods: {
         createContact() {
             console.log(this.form);
             const {...contact} = this.form // destructuring and rest operator :P Я их уже понимаю )  
+            this.contacts.push({...contact, id: Date.now(), isMarked: false}); 
             console.log(contact)
             this.form.name = this.form.value = '' // Clear fields after submit
+        },
+
+        markContact(id) {
+            // console.log(id);
+            const contact = this.contacts.find(c => c.id === id) 
+            contact.isMarked = true;
+        },
+
+        deleteContact(id) {
+            this.contacts = this.contacts.filter(c => c.id !== id)
         }
     }
 })
--- REST_API.Fullstack_App_on_JS/readme.md
+++ REST_API.Fullstack_App_on_JS/readme.md
@@ -1,4 +1,4 @@
 Video: https://www.youtube.com/watch?v=lzQIhjElV_g
 Nodemon: https://metanit.com/web/nodejs/2.6.php
 moesif-orign-cors-changer: https://chrome.google.com/webstore/detail/moesif-orign-cors-changer/digfbfaphojjndkpccljibejjbppifbc
-
+v-cloak: https://medium.com/vuejs-tips/v-cloak-45a05da28dc4
