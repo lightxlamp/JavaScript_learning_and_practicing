221ca258c32dd17360499b6c880ba3e4c14d04b4
lightxlamp <sheveliukhin.s@gmail.com>
2020-05-11 03:34:19 +0600

    Refactor

--- DevTools_Debugging/script.js
+++ DevTools_Debugging/script.js
@@ -1,26 +1,39 @@
-const num1 = document.querySelector('#num1');
-const num2 = document.querySelector('#num2');
-const btnAdd = document.querySelector('#add ');
-const btnSub = document.querySelector('#sub');
-const output = document.querySelector('#output');
+document.addEventListener('DOMContentLoaded', () => {
 
-function getInputValues() {
-    const val1 = num1.value; 
-    const val2 = num2.value; 
+    const num1 = document.querySelector('#num1');
+    const num2 = document.querySelector('#num2');
+    const btnAdd = document.querySelector('#add');
+    const btnSub = document.querySelector('#sub');
+    const output = document.querySelector('#output');
 
-    return [val1, val2] 
-}
+    function getInputValues() {
+        const val1 = +num1.value; 
+        const val2 = parseInt(num2.value); 
 
-function addHandler() {
-    const values = getInputValues();
-    const result = values[0] + values[1];
-    output.innerHTML = `Result =  ${result}`;
-}
+        return [val1, val2] 
+    }
 
-function subHandler() {
+    function addHandler() {
+        const values = getInputValues();
+        console.log(values);
+        const result = values[0] + values[1];
+        displayResult(result);
+        console.trace();
+    }
 
-}
+    function subHandler() {
+        const values = getInputValues();
+        const result = values[0] - values[1];
+        displayResult(result);
+    }
 
-btnAdd.addEventListener('click', addHandler)
-btnSub.addEventListener('click', subHandler)
+    function displayResult(result) {
+        output.closest('.card').style.display = 'block'; //классно не знал про Closest
+        output.innerHTML = `Result = ${result}`;
+    }
 
+    console.log(btnAdd);
+    btnAdd.addEventListener('click', addHandler)
+    btnSub.addEventListener('click', subHandler)
+
+});
