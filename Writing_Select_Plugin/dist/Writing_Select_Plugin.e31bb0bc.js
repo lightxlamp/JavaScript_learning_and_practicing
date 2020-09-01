cb2a7e0f82c3986ed9c6154d35d2d2535a0a0930
lightxlamp <sheveliukhin.s@gmail.com>
2020-09-02 00:59:10 +0600

    add selector

--- Writing_Select_Plugin/dist/Writing_Select_Plugin.e31bb0bc.js
+++ Writing_Select_Plugin/dist/Writing_Select_Plugin.e31bb0bc.js
@@ -240,7 +240,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53019" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49781" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
