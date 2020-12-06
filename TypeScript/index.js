18e149e31895c7a370f48325ff5e567a21646b12
Stanislav <sheveliukhin.s@gmail.com>
2020-12-07 01:22:39 +0600

    some more examples

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,5 +1,31 @@
-console.log('Hey loool');
+// https://www.youtube.com/watch?v=ahCwqrYpIuM&ab_channel=Fireship
 async function Authorize() {
     return true;
 }
-const url = new URL();
+const url = new URL('sa');
+// _.pick();
+let age = 23;
+let budget = 100; // opt out of the type system - отказаться
+let userName;
+age = 'male';
+budget = '100$';
+userName = 'Stas';
+userName = 1790;
+let font;
+font = 42;
+font = 'bolx';
+font = 'bold';
+const person1 = {
+    first: 'Stas',
+    last: 'Sheveliukhin'
+};
+const person2 = {
+    first: 'Nikitin',
+    last: 'Viktor',
+    age: 31,
+    parents: 'Galina'
+};
+const person3 = {
+    first: 'Tilek',
+    age: 33
+};
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,7 +1,49 @@
-console.log('Hey loool')
+// https://www.youtube.com/watch?v=ahCwqrYpIuM&ab_channel=Fireship
 
 async function Authorize() {
     return true;
 }
 
-const url = new URL();
+const url = new URL('sa');
+
+// _.pick();
+
+let age = 23;
+let budget:any = 100; // opt out of the type system - отказаться
+let userName : string
+
+age = 'male';
+budget = '100$'
+userName = 'Stas'
+userName = 1790
+
+type myType = 'bold' | 'italic' | 42;
+
+let font : myType
+
+font = 42
+font = 'bolx'
+font = 'bold'
+
+
+interface Person {
+    first : string, // required 
+    last: string, // required
+    [key: string]: any
+}
+
+const person1 : Person = {
+    first: 'Stas',
+    last: 'Sheveliukhin'
+}
+
+const person2 : Person = {
+    first: 'Nikitin',
+    last: 'Viktor',
+    age: 31,
+    parents: 'Galina'
+}
+const person3 : Person = {
+    first: 'Tilek',
+    age: 33
+}
