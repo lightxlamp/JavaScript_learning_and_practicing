f45d2abba0d33781a43871daecff10e08b8fb29a
Stanislav <sheveliukhin.s@gmail.com>
2020-12-13 03:48:41 +0600

    localstorage, trycatch

--- WebForMyself_JS/part_4/localstorage.js
+++ WebForMyself_JS/part_4/localstorage.js
@@ -1,14 +1,55 @@
+// // My code
+// document.querySelector('input[type="button"]').addEventListener('click', function() {
+//     let fieldValue = document.querySelector('input[type="text"]').value;
+//     if(fieldValue) {
+//       localStorage.setItem('fieldValue', fieldValue)
+//     }
+// })
+
+// My code
+// window.onload = function() {
+//     let field = document.querySelector('input[type="text"]');
+//     let savedFieldValue  = localStorage.getItem('fieldValue');
+//     if(savedFieldValue) {
+//         field.value = savedFieldValue;
+//     }
+// } 
+
+// Code from the lesson
 document.querySelector('input[type="button"]').addEventListener('click', function() {
     let fieldValue = document.querySelector('input[type="text"]').value;
+
+    var obj = {
+        text: fieldValue
+    }
+
     if(fieldValue) {
-      localStorage.setItem('fieldValue', fieldValue)
+      localStorage.setItem('fieldValue', JSON.stringify(obj))
     }
 })
 
-window.onload = function() {
+// Code from the lesson
+// document.addEventListener('DOMContentLoaded', function() {
+//     let field = document.querySelector('input[type="text"]');
+//     let savedFieldValue  = localStorage.getItem('fieldValue');
+//     if(savedFieldValue && savedFieldValue.trim()) {
+//         field.value = savedFieldValue;
+//     }
+// })
+
+// Code from the lesson
+document.addEventListener('DOMContentLoaded', function() {
     let field = document.querySelector('input[type="text"]');
-    let savedFieldValue  = localStorage.getItem('fieldValue');
-    if(savedFieldValue) {
-        field.value = savedFieldValue;
+    let obj; // undefined 
+    try {
+       obj  = JSON.parse(localStorage.getItem('fieldValue'));
     }
-} 
+    catch(e) {
+        obj = {}
+    }
+ 
+    // undefined.text 
+    if(obj && obj.text && obj.text.trim()) {
+        field.value = obj.text;
+    }
+})
