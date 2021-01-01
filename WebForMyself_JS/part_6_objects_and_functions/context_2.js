5f5337996331c0780213235b934cd9144ae368ab
Stanislav <sheveliukhin.s@gmail.com>
2021-01-01 18:53:57 +0600

    .

--- WebForMyself_JS/part_6_objects_and_functions/context_2.js
+++ WebForMyself_JS/part_6_objects_and_functions/context_2.js
@@ -25,6 +25,12 @@ let printPerson = printObj.bind(person) // as per bind return a new function wit
 printPerson('Person')
 printObj.bind(car)('Car')
 
+printObj.call(car, 'Car') // The same as bind but call immediately 
+
+printObj.apply(person, ['Person']) // like a call, but has only 2 arguments 
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
 // The for...in statement iterates over all enumerable properties of an object that are keyed by strings 
 // (ignoring ones keyed by Symbols), including inherited enumerable properties.
+
+// bind, call, apply used to be used before EcmaScript 6. But lost their actuality after it. Will now later why 
