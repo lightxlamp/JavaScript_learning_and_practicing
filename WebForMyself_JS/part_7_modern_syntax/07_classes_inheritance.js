65b08e0a0313ce7e4b31d8287f417c1bed00e06d
Stanislav <sheveliukhin.s@gmail.com>
2021-01-03 01:39:58 +0600

    add another class and documentation note

--- WebForMyself_JS/part_7_modern_syntax/07_classes_inheritance.js
+++ WebForMyself_JS/part_7_modern_syntax/07_classes_inheritance.js
@@ -9,6 +9,11 @@ class RootElement {
     hide() {
         this.$el.style.display = 'none';
     }
+
+    appendToWrapper() {
+        document.querySelector('.wrapper').insertAdjacentElement('afterbegin', this.$el)
+        return this;
+    }
 }
 
 
@@ -24,10 +29,34 @@ class Box extends RootElement {
         this.$el.style.backgroundColor = this.color
         this.$el.style.width = this.$el.style.height = `${this.size}px`
 
-        document.querySelector('.wrapper').insertAdjacentElement('afterbegin', this.$el)
-        return this;
+        this.appendToWrapper();
+    }
+}
+
+class Circle extends RootElement {
+    constructor(color, radius=100, tagName) {
+        super(tagName)
+        this.color = color
+        this.radius = radius 
+    }
+
+    create() {
+        this.$el.style.backgroundColor = this.color
+        this.$el.style.width = this.$el.style.height = `${this.radius}px`
+        this.$el.style.borderRadius = '50%' 
+
+        /* https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
+        'beforebegin': Before the targetElement itself.
+        'afterbegin': Just inside the targetElement, before its first child.
+        'beforeend': Just inside the targetElement, after its last child.
+        'afterend': After the targetElement itself. */
+        this.appendToWrapper();
     }
 }
 
 const redBox = new Box('red', 100, 'div').create();
 const blueBox = new Box('blue').create();
+const purpleCircle = new Circle('purple').create();
+
+const wrapperDiv = document.querySelector('.wrapper')
+wrapperDiv.insertAdjacentElement('beforebegin', document. createElement("p"))
