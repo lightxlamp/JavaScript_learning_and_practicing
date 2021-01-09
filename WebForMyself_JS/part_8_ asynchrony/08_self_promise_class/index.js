492fdd867e21b13359b5123b72dafc2fa771467d
Stanislav <sheveliukhin.s@gmail.com>
2021-01-10 02:46:54 +0600

    .

--- WebForMyself_JS/part_8_ asynchrony/08_self_promise_class/index.js
+++ WebForMyself_JS/part_8_ asynchrony/08_self_promise_class/index.js
@@ -11,28 +11,52 @@
 //     .then(num => num * 2)
 //     .catch(e => console.log(e))
 //     .then(num => num *= 3)
-//     ,then(console.log(num))
+//     .then(console.log(num))
 
 class MyPromise {
     constructor(callback) {
-        function resolver () {
 
+        this.onCatch = null; 
+        this.onFinally = null;
+        this.thens = [];
+
+        function resolver (data) {
+            this.thens.forEach(then => {
+                data = then(data)
+            });
         }
 
-        function rejected() {
-
+        function rejected(error) {
+            if(this.onCatch) {
+                this.onCatch(error);
+            }
+            if(this.onFinally) {
+                this.onFinally();
+            }
         }
 
         callback(resolver, rejected )
     }
 
     then(callbackFn) {
-
+        this.thens.push(callbackFn);
     }
     catch(callbackFn) {
-
+        this.onCatch = callbackFn;
     }
     finally(callbackFn) {
-
+        this.onFinally = callbackFn;
     }
 }
+
+
+const promise = new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(2)
+    }, 2000)
+})
+promise
+    .then(num => num * 2)
+    .catch(e => console.log(e))
+    .then(num => num *= 3)
+    .then(console.log(num))
