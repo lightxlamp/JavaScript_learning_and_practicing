7012ed4453424a216cac68be0ac4e699ee63e2e8
Stanislav <sheveliukhin.s@gmail.com>
2021-01-12 00:58:34 +0600

    build: setting up webpack

---
+++ WebForMyself_JS/part_9_JS_Blog_on_firebase/webpack.config.js
@@ -0,0 +1,25 @@
+// Writing on NodeJS platform here. So have additional features
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // require from node modules
+
+module.exports = {
+    // entry point of our app
+    entry: ['./src/index.js'],
+    // webpack output - result of webpack's work
+    output: {
+        path: __dirname + '/dist', // __dirname current folder's path
+        filename: 'bundle.js' // scripts + modules... Good practice to call it bundle
+    },
+    devServer: {
+        contentBase: __dirname + '/dist' //path to folder where we need to run our dev server
+    },
+    // to manipulate HTML (index.html), and add our bundle to it we need HtmlWebpackPlugin
+    // plugins: [new HtmlWebpackPlugin()] // generates a new index.html. Like a did with documentation
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: '.src/index.html'
+    })],
+    resolve: {
+        extensions: ['.js']
+    }
+}
