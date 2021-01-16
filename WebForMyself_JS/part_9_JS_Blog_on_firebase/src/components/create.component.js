3db8512c622ef810a7be0fa3538a2de17c2d526e
lightxlamp <sheveliukhin.s@gmail.com>
2021-01-17 03:21:45 +0600

    feat: create form

--- WebForMyself_JS/part_9_JS_Blog_on_firebase/src/components/create.component.js
+++ WebForMyself_JS/part_9_JS_Blog_on_firebase/src/components/create.component.js
@@ -1,7 +1,29 @@
 import { Component } from "../core/component";
+import { Form } from '../core/form'
 
 export class CreateComponent extends Component {
     constructor(id) {
         super(id)
     }
+
+    init() {
+        this.$el.addEventListener('submit', submitHandler.bind(this))
+
+        // 'Angular-like' validation
+        this.form = new Form(this.$el, {
+            title: [],
+            fullText: []
+        });
+    }
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+
+    const formData = {
+        type: this.$el.type.value,
+        ...this.form.value()
+    }
+    // console.log(formData, this.form.value());
+    console.log('Submit', formData)
 }
---
+++ WebForMyself_JS/part_9_JS_Blog_on_firebase/src/core/form.js
@@ -0,0 +1,18 @@
+export class Form {
+    constructor(form, controls) {
+        this.form = form;
+        this.controls = controls;
+    }
+
+    value() {
+        const value = {}
+
+        console.log('this.controls', this.controls);
+         
+        Object.keys(this.controls).forEach(control => {
+            value[control] = this.form[control].value
+        })
+
+        return value
+    }
+}
