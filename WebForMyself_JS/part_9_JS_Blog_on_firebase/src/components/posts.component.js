d01e294523fd8d19331cd9f9bf935084fabbae9f
lightxlamp <sheveliukhin.s@gmail.com>
2021-01-18 03:22:49 +0600

    feat: render list of posts

--- WebForMyself_JS/part_9_JS_Blog_on_firebase/src/components/posts.component.js
+++ WebForMyself_JS/part_9_JS_Blog_on_firebase/src/components/posts.component.js
@@ -12,6 +12,45 @@ export class PostsComponent extends Component {
         console.log(fireBasePosts);
         const arrayOfPosts = TransformService.fireBaseObjectToArray(fireBasePosts)
         console.log(arrayOfPosts);
+
+        console.log('this', this);
+
+        // my code without watching lesson
+        // arrayOfPosts.forEach(post => {
+        //     this.$el.insertAdjacentHTML('afterbegin', renderPost(post));
+        // })
+
+        // lesson code a little bit clearer 
+        const html = arrayOfPosts.map(post => renderPost(post))
+        this.$el.insertAdjacentHTML('afterbegin', html.join(' '));
+    }
+
+    onHide() {
+        this.$el.innerHTML = '' // so bad.. May be this will be rewritten. Optimized. We could use loaded data
     }
 }
 
+function renderPost(post) {
+    const tag = post.type === 'news'
+        ? '<li class="tag tag-blue tag-rounded">News</li>'
+        : '<li class="tag tag-rounded">Note</li>'
+
+    const button = '<button class="button-primary button-small button-round">Save</button>'; // https://kylelogue.github.io/mustard-ui/index.html
+        
+    return `    
+    <div class="panel">
+        <div class="panel-head">
+            <p class="panel-title">${post.title}</p>
+            <ul class="tags">
+                ${tag}
+             </ul>
+        </div>
+        <div class="panel-body">
+            <p class="multi-line">${post.fullText}</p>
+        </div>
+        <div class="panel-footer w-panel-footer">
+            <small>${post.date}</small>
+            ${button}
+        </div>
+  </div>`
+}
