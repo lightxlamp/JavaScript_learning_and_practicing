51c7e055a17296f37e998794690b098b3492c5f1
lightxlamp <sheveliukhin.s@gmail.com>
2021-02-15 01:12:24 +0600

    rewritten with another logic

--- WebForMyself_JS/self_testing/some_logic/src/components/tabs.component.js
+++ WebForMyself_JS/self_testing/some_logic/src/components/tabs.component.js
@@ -6,7 +6,18 @@ export default class Tab extends Component {
     }
     
     init() {
+        this.$el.addEventListener('click', tabClickHandler)
         // this.$el.addEventListener('click', tabClickHandler.bind(this))
-        this.$el.addEventListener('click', alert('Init called'))
+        // this.$el.addEventListener('click', alert('Init called'))
     }
+}
+
+function tabClickHandler(e) { 
+    // console.log(e.target);
+    // console.log('this', this.querySelectorAll('.tabs'));
+    this.querySelectorAll('.tabs .tab').forEach(tab => {
+        console.log('tab', tab);
+        tab.classList.remove('active')
+    });
+    e.target.classList.add('active')
 }
