b2af41093a7beebca45ad38a19e15a9776c06298
lightxlamp <sheveliukhin.s@gmail.com>
2021-10-31 02:47:37 +0600

    feat: redefine "remove item" as in video. But mine was more efficient

--- Vanilla-js-modal-window/index.js
+++ Vanilla-js-modal-window/index.js
@@ -1,4 +1,4 @@
-const games = [
+let games = [
     {id: 1, title: 'Gothic II: Gold Edition', price: 5.99, img: './img/gothic2.jpeg', desc: 'Gothic II: Gold Edition brings together the excitement of Gothic II and the add-on Night of the Raven to your fingertips! <br/> You have torn down the magical barrier and released the prisoners of the Mine Valley. Now the former criminals of the forests and mountains are causing trouble around the capital of Khorinis. The town militia is powerless due to their low amount of force–outside of the town, everyone is helpless against the attacks of the bandits.'},
     {id: 2, title: 'Theft Auto V: Premium Edition', price: 12.57, img: './img/gta5.jpeg', desc: 'When a young street hustler, a retired bank robber and a terrifying psychopath find themselves entangled with some of the most frightening and deranged elements of the criminal underworld, the U.S. government and the entertainment industry, they must pull off a series of dangerous heists to survive in a ruthless city in which they can trust nobody, least of all each other.'},
     {id: 3, title: 'Remnant: From the Ashes - Complete Edition', price: 13.11, img: './img/remnant.jpeg', desc: 'Remnant: From the Ashes is a third-person survival action shooter set in a post-apocalyptic world overrun by monstrous creatures. As one of the last remnants of humanity, you’ll set out alone or alongside up to two other players to face down hordes of deadly enemies and epic bosses, and try to carve a foothold, rebuild, and then retake what was lost.'}
@@ -67,7 +67,9 @@ document.addEventListener('click', event => {
             removePromise.then(
                 () => {
                     console.log('Resolve');
-                    document.querySelector(`.card[data-good-id="${goodId}"]`).remove();
+                    // document.querySelector(`.card[data-good-id="${goodId}"]`).remove();
+                    games = games.filter(game => game.id !== goodId)
+                    render(); // I like my solution more (2 rows above). It is in 1 line. And seems like more efficient
                 }).catch(
                     () => {
                         console.log('Reject');
