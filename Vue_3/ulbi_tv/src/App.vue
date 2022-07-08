7a421d98194442758756b51e9c6498c2b5b7f866
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-07-09 00:47:30 +0600

    feat: test likes/dislikes. Data/methods

--- Vue_3/ulbi_tv/src/App.vue
+++ Vue_3/ulbi_tv/src/App.vue
@@ -1,11 +1,36 @@
 <template>
+  <div class="app_wrapper">
     <h1>Go to bad, Stas :)</h1>
+    <div>
+      Likes count: <strong>{{ this.likes }}</strong> Dislikes count:
+      <strong>{{ this.dislikes }}</strong>
+    </div>
+    <br />
+    <button @click="like()">Like</button>
+    <br />
+    <br />
+    <button @click="dislike()">Dislike</button>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    like() {
+      this.likes++;
+    },
+    dislike() {
+      this.dislikes++;
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
