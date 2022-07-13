77bfe9eb8a8489a3430de6f8fc6d7437a651f98c
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-07-14 00:02:38 +0600

    study: assign values

--- Vue_3/ulbi_tv/src/components/PostForm.vue
+++ Vue_3/ulbi_tv/src/components/PostForm.vue
@@ -3,22 +3,43 @@
     <input
       type="text"
       placeholder="Title"
-      v-bind:value="title"
-      @input="title = $event.target.value"
+      v-bind:value="post.title"
+      @input="post.title = $event.target.value"
     />
     <br />
     <input
       type="text"
       placeholder="Body"
-      v-bind:value="body"
-      @input="body = $event.target.value"
+      v-bind:value="post.body"
+      @input="post.body = $event.target.value"
     />
     <br />
     <button>Create post</button>
   </form>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+};
+</script>
+
 <style scoped>
+form {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 2px solid black;
+  border-radius: 10px;
+}
+
 input {
   margin: 10px 0;
   width: 100%;
