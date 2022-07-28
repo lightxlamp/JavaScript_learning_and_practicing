9fab30c3cb9a7e4d2e7f57a5988ab228876466cf
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-07-28 23:20:41 +0600

    study: add animation

--- Vue_3/ulbi_tv/src/components/PostList.vue
+++ Vue_3/ulbi_tv/src/components/PostList.vue
@@ -2,12 +2,14 @@
   <div v-if="posts.length > 0">
     <h1>List of posts</h1>
     <!-- https://learnvue.co/tutorials/vue-for-loop-tips -->
-    <PostItem
-      v-for="post in posts"
-      :post="post"
-      :key="post.id"
-      @deletePost="deletePost(post)"
-    ></PostItem>
+    <TransitionGroup name="posts-list">
+      <PostItem
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @deletePost="deletePost(post)"
+      ></PostItem>
+    </TransitionGroup>
   </div>
   <div v-else>
     <h1>No posts yet</h1>
@@ -39,5 +41,17 @@ h1 {
   text-align: center;
   margin: 2rem 0;
   text-transform: uppercase;
+}
+.posts-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.posts-list-enter-active,
+.posts-list-leave-active {
+  transition: all 10s;
+}
+.posts-list-enter, .posts-list-leave-to /* .posts-list-leave-active before v2.1.8 */ {
+  opacity: 1;
+  transform: translateY(30px);
 }
 </style>
