e5c02375632cec2b1bda4d1859e7098a44bf6446
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2023-03-05 00:54:15 +0600

    study: Fix SW registration. Icon errors. Experimenting

--- PWA/js/app.js
+++ PWA/js/app.js
@@ -1,10 +1,11 @@
 window.addEventListener('load', async () => {
   if ('serviceWorker' in navigator) {
     try {
-      const reg = await navigator.serviceWorker.register('/sw.js')
+      console.log("'serviceWorker' in navigator)", 'serviceWorker' in navigator);
+      const reg = await navigator.serviceWorker.register('./sw.js')
       console.log('Service worker register success', reg)
     } catch (e) {
-      console.log('Service worker register fail')
+      console.log('Service worker register failed')
     }
   }
 
