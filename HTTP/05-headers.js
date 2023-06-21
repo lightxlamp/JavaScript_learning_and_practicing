acf550904863b5ba8d52c6a254569e65ac466acb
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2023-06-22 00:31:53 +0600

    study: Log content type

--- HTTP/05-headers.js
+++ HTTP/05-headers.js
@@ -1,13 +1,22 @@
 function logContentType(resp) {
-    // ?
+    console.log(resp)
+    for (const prop in resp) {
+        console.log(prop + ':', resp[prop]);
+    }
+    console.log(resp.headers)
+    console.log(resp.headers.get("Content-Type"))
 }
 
-const apiKey = generateKey()
-const bootdevAPIDomain = 'api.boot.dev'
+const apiKey = generateKey();
+const bootdevAPIDomain = 'api.boot.dev';
 
-const items = await getItemData(bootdevAPIDomain)
-
-logContentType(items)
+getItemData(bootdevAPIDomain)
+    .then(items => {
+        logContentType(items);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
 async function getItemData(domain) {
     const response = await fetch(`https://${domain}/v1/courses_rest_api/learn-http/items/0194fdc2-fa2f-4cc0-81d3-ff12045b73c8`, {
@@ -17,15 +26,15 @@ async function getItemData(domain) {
             'X-API-Key': apiKey,
             'Content-Type': 'application/json'
         }
-    })
-    return response
+    });
+    return response;
 }
 
 function generateKey() {
-    const characters = 'ABCDEF0123456789'
-    let result = ''
+    const characters = 'ABCDEF0123456789';
+    let result = '';
     for (let i = 0; i < 16; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length))
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
-    return result
+    return result;
 }
