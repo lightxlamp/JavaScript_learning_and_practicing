762b78c993c6a750af6afd5b17734ef08152bca2
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2023-07-08 23:19:22 +0600

    study: json parse

---
+++ HTTP/09-json-parse.js
@@ -0,0 +1,38 @@
+function parseLocation(locationString) {
+    try {
+        const parsedLocation = JSON.parse(locationString)
+        printLocationObj(parsedLocation)
+    }
+    catch (error) {
+        console.log('⚠️ Invalid JSON string ⚠️')
+    }
+}
+
+// don't touch below this line
+
+function printLocationObj(parsed) {
+    console.log(`id: ${parsed.id}`)
+    console.log(`discovered: ${parsed.discovered}`)
+    console.log(`name: ${parsed.name}`)
+    console.log(`recommendedLevel: ${parsed.recommendedLevel}`)
+}
+
+parseLocation(`
+{
+	"discovered",
+	"id": "0194fdc2-fa2f-4cc0-81d3-ff12045b73c8",
+	"name": "Bandit Camp",
+	"recommendedLevel": 14
+}
+`)
+
+console.log('---')
+
+parseLocation(`
+{
+	"discovered": false,
+	"id": "0194fdc2-fa2f-4cc0-81d3-ff12045b73c8",
+	"name": "Bandit Camp",
+	"recommendedLevel": 14
+}
+`)
