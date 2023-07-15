4dca339a72cbe74fefc97a658b1e59a3f3609bb7
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2023-07-16 05:20:00 +0600

    study: fill jest test with logic. Add 2nd one

--- HTTP/web-crawler/crawl.js
+++ HTTP/web-crawler/crawl.js
@@ -1,7 +1,15 @@
 function normalizeURL(urlString) {
-    return urlString
+  const urlObject = new URL(urlString);
+  const hostPath = `${urlObject.hostname}${urlObject.pathname}`;
+  // console.log(urlObject)
+  if(hostPath.length > 0 && hostPath.slice(-1) === '/') {
+    return hostPath.slice(0, -1)
+  }
+  return hostPath
 }
 
 module.exports = {
-    normalizeURL
-}
+  normalizeURL,
+};
+
+// normalizeURL('https://blog.boot.dev/path')
--- HTTP/web-crawler/crawl.test.js
+++ HTTP/web-crawler/crawl.test.js
@@ -2,9 +2,16 @@ const { normalizeURL } = require("./crawl.js");
 
 const { test, expect } = require("@jest/globals");
 
-test('normalizeURL', () => {
-    const input = ''
+test('normalizeURL: strip protocol', () => {
+    const input = 'https://blog.boot.dev/path'
     const actual = normalizeURL(input)
-    const expected = ''
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL: remove trailing slashes', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
 })
