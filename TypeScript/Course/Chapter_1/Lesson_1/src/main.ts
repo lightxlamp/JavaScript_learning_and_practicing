4a69e445a4123863399a571a751f7df5fe4f4f29
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2024-04-20 23:57:11 +0600

    TS. Copy 2nd lesson structure. Change code of 1st and 2nd lessons in places

--- TypeScript/Course/Chapter_1/Lesson_1/src/main.ts
+++ TypeScript/Course/Chapter_1/Lesson_1/src/main.ts
@@ -1,29 +1,9 @@
-let myName: string = "Stas"
-let yearOfBirth: number;
-let isLoading: boolean;
-let album: any; // ℹ️ In some way it will defeat TypeScript :p
-// ℹ️ If you are not sure - what type of data you will be receiving
-let sex: number | string
-let postId: string | number
-let isActive: boolean | number // True / False. 1 / 0
-let megaUnionTypeVariable: boolean | string | number
-const re: RegExp = /ab+c/;
+let username = "Stas"
+console.log(username)
 
-// ⚠️ myName = 34; // Type number is not assignable to type string
-myName = "Viktor"
-console.log(myName)
-yearOfBirth = 1989
-isLoading = true
-album = 321
-sex = 1
-console.log(sex)
-sex = 'male'
-// ⚠️ sex = true // Type boolean is not assignable to type string | number
+const a: number = 2
+const b: number = 10
+const c: number = 5
 
-const sum = (a:number, b:number) => {
-    return a + b
-}
-
-const sum2 = (a:number, b:string) => {
-    return a + b
-}
+console.log(a * b)
+console.log(b / c)
---
+++ TypeScript/Course/Chapter_1/Lesson_3/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = "Stas";
+console.log(username);
+const a = 2;
+const b = 10;
+const c = 5;
+console.log(a * b);
+console.log(b / c);
