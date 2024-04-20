14950bcb61ca1009597b534bf4c24ce33520fc9d
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2024-04-20 23:54:17 +0600

    TS. Basic Types. Union type

--- TypeScript/Course/Chapter_1/Lesson_1/src/main.ts
+++ TypeScript/Course/Chapter_1/Lesson_1/src/main.ts
@@ -1,9 +1,29 @@
-let username = "Stas Chapter 1"
-console.log(username)
+let myName: string = "Stas"
+let yearOfBirth: number;
+let isLoading: boolean;
+let album: any; // ℹ️ In some way it will defeat TypeScript :p
+// ℹ️ If you are not sure - what type of data you will be receiving
+let sex: number | string
+let postId: string | number
+let isActive: boolean | number // True / False. 1 / 0
+let megaUnionTypeVariable: boolean | string | number
+const re: RegExp = /ab+c/;
 
-const a: number = 2
-const b: number = 10
-const c: number = 5
+// ⚠️ myName = 34; // Type number is not assignable to type string
+myName = "Viktor"
+console.log(myName)
+yearOfBirth = 1989
+isLoading = true
+album = 321
+sex = 1
+console.log(sex)
+sex = 'male'
+// ⚠️ sex = true // Type boolean is not assignable to type string | number
 
-console.log(a * b)
-console.log(b / c)
+const sum = (a:number, b:number) => {
+    return a + b
+}
+
+const sum2 = (a:number, b:string) => {
+    return a + b
+}
