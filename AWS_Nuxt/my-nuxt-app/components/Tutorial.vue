14346e8843ce896f4c9080c51a9d88cb8ebc6578
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2024-10-20 21:32:50 +0600

    Add errro generator BTN. Generate project

--- AWS_Nuxt/my-nuxt-app/components/Tutorial.vue
+++ AWS_Nuxt/my-nuxt-app/components/Tutorial.vue
@@ -13,6 +13,7 @@
         <p class="mt-3 text-gray-600">
           We recommend you take a look at the <a href="https://nuxtjs.org" target="_blank" class="button--doc text-green-500 hover:underline">Nuxt documentation</a>, whether you are new or have previous experience with the framework.<br>
         </p>
+        <button @click="triggerError">Generate Error</button>
         <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
           To get started, remove <code class="bg-gray-100 text-sm p-1 rounded border">components/Tutorial.vue</code> and start coding in <code class="bg-gray-100 text-sm p-1 rounded border">pages/index.vue</code>. Have fun!
         </p>
@@ -47,6 +48,12 @@
 
 <script>
 export default {
-  name: 'NuxtTutorial'
+  name: 'NuxtTutorial',
+  methods: {
+    triggerError() {
+      Object.values(null);
+    },
+  }
 }
+
 </script>
