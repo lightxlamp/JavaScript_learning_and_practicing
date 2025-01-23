7fcfe845108ddc467c51ab7be47ca1ad250777a0
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2025-01-23 16:58:08 +0600

    previos lessons

--- Vue_3_Traversy_Media/1st/Vue.js Crash Course 2024/src/App.vue
+++ Vue_3_Traversy_Media/1st/Vue.js Crash Course 2024/src/App.vue
@@ -1,9 +1,34 @@
-<script setup>
-
+<script>
+  export default {
+    data() {
+      return {
+        name: 'Stas',
+        status: 'pending',
+        tasks: ['Task 1', 'Task 2', 'Task 3', 'Task 4'],
+        link: 'https://www.youtube.com/watch?v=VeNfHj6MhgA'
+      }
+    }
+  }
 </script>
 
 <template>
-  <h1>...</h1>
+  <h1>{{ name }}</h1>
+  <br />
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  <br />
+  <br />
+
+  <p v-for="task in tasks" :key="task">
+     {{ task  }}
+  </p>
+
+  <br />
+  <br />
+  
+  <a v-bind:href="link">Link to this video</a>
+
 </template>
 
 <style scoped>
