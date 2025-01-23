73ccdc5e5de89c5229738eeb9163e0f51b42720c
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2025-01-23 18:26:24 +0600

    That's all for Options API

--- Vue_3_Traversy_Media/1st/Vue.js Crash Course 2024/src/App.vue
+++ Vue_3_Traversy_Media/1st/Vue.js Crash Course 2024/src/App.vue
@@ -1,14 +1,41 @@
 <script>
-  export default {
-    data() {
-      return {
-        name: 'Stas',
-        status: 'pending',
-        tasks: ['Task 1', 'Task 2', 'Task 3', 'Task 4'],
-        link: 'https://www.youtube.com/watch?v=VeNfHj6MhgA'
+export default {
+  data() {
+    return {
+      name: "Stas",
+      status: "pending",
+      tasks: ["Task 1", "Task 2", "Task 3", "Task 4"],
+      link: "https://www.youtube.com/watch?v=VeNfHj6MhgA",
+    };
+  },
+  methods: {
+    changeStatus_switch() {
+      switch (this.status) {
+        case 'active':
+          this.status = 'pending'
+          break;
+        case 'pending':
+          this.status = 'inactive'
+          break;
+        case 'inactive':
+          this.status = 'active'
+          break;
+        default:
+          console.error('Some wrong status')
       }
-    }
-  }
+    },
+
+    changeStatus() {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+      } else {
+        this.status = "active";
+      }
+    },
+  },
+};
 </script>
 
 <template>
@@ -18,19 +45,25 @@
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
   <br />
+  <button @click="changeStatus_switch">Change status</button>
+  <br />
   <br />
 
-  <p v-for="task in tasks" :key="task">
-     {{ task  }}
-  </p>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 
   <br />
-  <br />
-  
+
   <a v-bind:href="link">Link to this video</a>
+  <br />
+  <br />
+
+  <a :href="link">Link to this video</a>
+
+  <br />
+  <br />
 
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
