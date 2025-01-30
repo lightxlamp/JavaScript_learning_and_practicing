ddf8e486f8616a532f18e4e6e15d43f43485ede0
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2025-01-30 19:31:59 +0600

    Add card section with Slots

--- Vue_3_Traversy_Media/2nd/vue-jobs-new/src/App.vue
+++ Vue_3_Traversy_Media/2nd/vue-jobs-new/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import Navbar from './components/Navbar.vue';
   import Hero from './components/Hero.vue';
+  import HomeCards from './components/HomeCards.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero title="Become a Vue Dev" subtitle="Find the Vue job that fits your skills and needs" />
+  <HomeCards />
 
 </template>
 
---
+++ Vue_3_Traversy_Media/2nd/vue-jobs-new/src/components/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { defineProps } from 'vue'; 
+    defineProps(
+        {
+            bg: {
+                type: String,
+                default: 'bg-gray-100'
+            }
+        }
+    )
+</script>
+
+<template>
+  <!-- <div class="p-6 rounded-lg shadow-md" :class="bg"> -->
+  <div :class="`${bg} p-6 rounded-lg shadow-md`"> <!-- Remembered this approach.. -->
+    <slot></slot>
+  </div>
+</template>
