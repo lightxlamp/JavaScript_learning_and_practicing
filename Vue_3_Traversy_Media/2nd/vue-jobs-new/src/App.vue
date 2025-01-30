c114772387ccc8a7ebf586f5772cad8c9263ffdc
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2025-01-30 19:43:12 +0600

    Jobs list started

--- Vue_3_Traversy_Media/2nd/vue-jobs-new/src/App.vue
+++ Vue_3_Traversy_Media/2nd/vue-jobs-new/src/App.vue
@@ -2,13 +2,14 @@
   import Navbar from './components/Navbar.vue';
   import Hero from './components/Hero.vue';
   import HomeCards from './components/HomeCards.vue';
+  import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero title="Become a Vue Dev" subtitle="Find the Vue job that fits your skills and needs" />
   <HomeCards />
-
+  <JobListings />
 </template>
 
 <style scoped>
---
+++ Vue_3_Traversy_Media/2nd/vue-jobs-new/src/components/JobListings.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import jobsData from '../jobs.json'
+    import { ref } from 'vue';
+
+    const jobs = ref(jobsData);
+</script>
+
+<template>
+    <div v-for="job in jobs" :key="job.id">{{ job.title }}</div>
+</template>
