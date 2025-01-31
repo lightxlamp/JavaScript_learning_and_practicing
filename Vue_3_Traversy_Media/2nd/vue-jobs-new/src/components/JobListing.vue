768851d805814fa070bb5605ce803a9e4ffbde22
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2025-01-31 12:17:02 +0600

    Experimenting: Add broken test data object. Add hadnling for it

--- Vue_3_Traversy_Media/2nd/vue-jobs-new/src/components/JobListing.vue
+++ Vue_3_Traversy_Media/2nd/vue-jobs-new/src/components/JobListing.vue
@@ -13,22 +13,29 @@ defineProps({
   <div class="bg-white rounded-xl shadow-md relative">
     <div class="p-4">
       <div class="mb-6">
-        <div class="text-gray-600 my-2">{{ job.type }}</div>
-        <h3 class="text-xl font-bold">{{ job.title }}</h3>
+        <div class="text-gray-600 my-2">{{ job?.type || "Not specified" }}</div>
+        <h3 class="text-xl font-bold">
+          {{ job?.title || "No title provided" }}
+        </h3>
       </div>
 
-      <div class="mb-5">{{ job.description }}</div>
+      <div class="mb-5">
+        {{ job?.description || "No description available." }}
+      </div>
 
-      <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
+      <h3 class="text-green-500 mb-2">
+        {{ job?.salary ? job.salary + " / Year" : "Salary not specified" }}
+      </h3>
 
       <div class="border border-gray-100 mb-5"></div>
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
           <i class="fa-solid fa-location-dot text-lg"></i>
-          {{ job.location }}
+          {{ job?.location || "Location not specified" }}
         </div>
         <a
+          v-if="job?.id"
           :href="'/job/' + job.id"
           class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
