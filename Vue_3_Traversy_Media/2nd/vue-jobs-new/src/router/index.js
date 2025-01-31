aaac69175350b6811212cf5161e5b5efbf3cfa53
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2025-01-31 15:53:27 +0600

    Configure router up to the end. Fix typos

--- Vue_3_Traversy_Media/2nd/vue-jobs-new/src/router/index.js
+++ Vue_3_Traversy_Media/2nd/vue-jobs-new/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHostory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter(
     {
-        history: createWebHostory(import.meta.env.BASE_URL), 
+        history: createWebHistory(import.meta.env.BASE_URL), 
         routes: [
             {
                 path: '/',
