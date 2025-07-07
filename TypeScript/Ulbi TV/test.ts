360a5a9d82efc409c027841a0d82c0531bacdef8
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2025-07-07 15:40:34 +0600

    Adjust TS

---
+++ TypeScript/Ulbi TV/test.ts
@@ -0,0 +1,9 @@
+const age : number = 23
+
+let value : any = 23
+value = '23'
+value = true
+
+function logData(data) { // noImplicitAny: false - That's why we have error here
+    console.log(data)
+}
