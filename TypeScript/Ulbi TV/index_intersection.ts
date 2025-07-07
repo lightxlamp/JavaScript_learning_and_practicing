acb6dd6425e148d644aa11dee713ea51350a58ff
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2025-07-07 15:08:02 +0600

    Add TS

---
+++ TypeScript/Ulbi TV/index_intersection.ts
@@ -0,0 +1,14 @@
+// type mainInfo = {
+//     firstName: string,
+//     lastName: string,
+// }
+
+// type AdditionalInfo = {
+//     age: number,
+// }
+
+// type FullInfo = mainInfo & AdditionalInfo
+
+// const info0 : FullInfo = {firstName: 'Stas', lastName: 'Sheveliukhin', age: 35}
+// const info1: FullInfo = {firstName: 'Stas', lastName: 'Sheveliukhin'} // Error 
+// const info2: FullInfo = {age: 35} // Error 
---
+++ TypeScript/Ulbi TV/index_union.ts
@@ -0,0 +1,14 @@
+// type mainInfo = {
+//     firstName: string,
+//     lastName: string,
+// }
+
+// type AdditionalInfo = {
+//     age: number,
+// }
+
+// type FullInfo = mainInfo | AdditionalInfo
+
+// const info0 : FullInfo = {firstName: 'Stas', lastName: 'Sheveliukhin', age: 35}
+// const info1: FullInfo = {firstName: 'Stas', lastName: 'Sheveliukhin'}
+// const info2: FullInfo = {age: 35}
