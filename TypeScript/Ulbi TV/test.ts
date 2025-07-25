edf19f47cca37f81a26458e4dce27cc59836283f
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2025-07-25 12:01:20 +0600

    Fix comment

--- TypeScript/Ulbi TV/test.ts
+++ TypeScript/Ulbi TV/test.ts
@@ -4,6 +4,6 @@ let value : any = 23
 value = '23'
 value = true
 
-function logData(data) { // noImplicitAny: false - That's why we have error here
+function logData(data) { // noImplicitAny: true - That's why we have error here
     console.log(data)
 }
